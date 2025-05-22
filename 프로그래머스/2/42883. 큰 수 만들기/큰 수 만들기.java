76c46b2de66521f4c00db08edd8e4a10f5debc13
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-05-22 17:05:37 +0900

    [level 2] Title: 큰 수 만들기, Time: 9967.38 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String number, int endIndex){
+        StringBuilder answer = new StringBuilder();
+        int maxIndex;
+        int startIndex = 0;
+        
+
+        while(endIndex < number.length()){
+            maxIndex = startIndex;
+            for(int i=startIndex+1; i<=endIndex; i++){
+                if(number.charAt(i) > number.charAt(maxIndex))
+                    maxIndex = i;
+            }
+            answer.append(number.charAt(maxIndex));
+            endIndex++;
+            startIndex = maxIndex + 1;
+        }
+
+        return answer.toString();
+    }
+}
